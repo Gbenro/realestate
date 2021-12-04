912bbe0c7ca79f7ce00d8073ea0f1f36eae9abde
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2021-12-04 12:35:46 -0600

    added properties search

---
+++ realestate/components/Layout.jsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import { Box } from "@chakra-ui/layout";
+import { Children } from "react";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Real Estate</title>
+      </Head>
+      <Box maxWidth="1280px" m="auto">
+        <header>
+          <NavBar />
+        </header>
+        <main>{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </Box>
+    </>
+  );
+};
+
+export default Layout;
