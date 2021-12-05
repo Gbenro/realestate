3988be820cc8c1adc2ca581355ad00baa02144ae
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2021-12-05 16:38:26 -0600

    completed realtor website

--- realestate/components/SearchFilters.jsx
+++ realestate/components/SearchFilters.jsx
@@ -12,9 +12,48 @@ import {
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
+import { FcLikePlaceholder } from "react-icons/fc";
 
 const SearchFilters = () => {
-  return <> search Filters</>;
+  const router = useRouter();
+  const [filters, setFilters] = useState(filterData);
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value;
+      }
+    });
+    router.push({ pathname: path, query });
+  };
+
+  return (
+    <Flex bg="grey.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            w="fit-content"
+            p="2"
+            placeholder={filter.placeholder}
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
